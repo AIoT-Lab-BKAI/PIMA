d1995f8cda9805edff3c25711e968f0324c2d11f
thanhhff <thanhnguyen200399@gmail.com>
2022-01-01 17:13:24 +0700

    Update code

--- models/prescription_pill.py
+++ models/prescription_pill.py
@@ -9,29 +9,28 @@ from models import ImageEncoder, ProjectionHead, BERTxSAGE, ImageEncoderTimm, se
 class PrescriptionPill(nn.Module):
     def __init__(self, image_embedding=CFG.image_embedding, text_embedding = CFG.text_embedding, graph_embedding=CFG.graph_embedding, graph_n_classes=len(CFG.LABELS), image_pretrained_link=CFG.image_pretrained_link):
         super().__init__()
-        # self.image_encoder = ImageEncoderTimm()
-        self.image_encoder = ImageEncoder()
+        self.image_encoder = ImageEncoderTimm()
 
-        if image_pretrained_link is not None:
-            pre_train_state = torch.load(image_pretrained_link)
-            image_model_state = self.image_encoder.state_dict()
-            # 1. filter out unnecessary keys
-            pretrained_dict = {k: v for k, v in pre_train_state.items() if k in image_model_state}
-            # 2. overwrite entries in the existing state dict
-            image_model_state.update(pretrained_dict)
-            # 3. load the new state dict
-            self.image_encoder.load_state_dict(image_model_state)
-            print("Loaded pretrained image model successfully!")
+        # self.image_encoder = ImageEncoder()
+        # if image_pretrained_link is not None:
+        #     pre_train_state = torch.load(image_pretrained_link)
+        #     image_model_state = self.image_encoder.state_dict()
+        #     # 1. filter out unnecessary keys
+        #     pretrained_dict = {k: v for k, v in pre_train_state.items() if k in image_model_state}
+        #     # 2. overwrite entries in the existing state dict
+        #     image_model_state.update(pretrained_dict)
+        #     # 3. load the new state dict
+        #     self.image_encoder.load_state_dict(image_model_state)
+        #     print("Loaded pretrained image model successfully!")
 
-        # self.text_encoder = TextEncoder()
-        self.graph_encoder = SBERTxSAGE()
-        self.graph_sentences_encoder = sentencesTransformer()
+        # self.graph_encoder = SBERTxSAGE()
+        # self.graph_sentences_encoder = sentencesTransformer()
 
         self.sentences_encoder = sentencesTransformer()
 
         self.image_projection = ProjectionHead(embedding_dim=image_embedding)
         self.text_projection = ProjectionHead(embedding_dim=text_embedding)
-        self.graph_projection = ProjectionHead(embedding_dim=graph_embedding)
+        # self.graph_projection = ProjectionHead(embedding_dim=graph_embedding)
 
     #     self.post_process_layers = nn.Sequential(
     #         nn.BatchNorm1d(256, affine=False),
