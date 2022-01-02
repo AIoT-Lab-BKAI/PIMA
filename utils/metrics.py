a07e5f8fbdfc1e9c8fd7fb45dcf6bd1fd77e7d79
sudo-u-Hung-NN <hung.nn184118@sis.hust.edu.vn>
2022-01-02 16:08:24 +0700

    Update contrative loss

--- utils/metrics.py
+++ utils/metrics.py
@@ -46,3 +46,24 @@ class TripletLoss(nn.Module):
         losses = torch.relu( - distance_positive + distance_negative + self.margin)
 
         return losses.mean()
+
+class ContrastiveLoss(nn.Module):
+    """
+    http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+        self.cos = nn.CosineSimilarity(dim=-1, eps=1e-6)
+    
+    def calc_cosinsimilarity(self, x1, x2):
+        return self.cos(x1, x2)
+
+    def forward(self, anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor) -> torch.Tensor:
+        distance_positive = self.calc_cosinsimilarity(anchor, positive)
+        distance_negative = self.calc_cosinsimilarity(anchor, negative)
+
+        loss_contrastive = torch.pow(distance_negative, 2) + torch.pow(torch.relu(self.margin - distance_positive), 2)
+
+        return torch.mean(loss_contrastive)
