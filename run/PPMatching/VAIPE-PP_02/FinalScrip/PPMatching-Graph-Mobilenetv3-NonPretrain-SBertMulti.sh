aaa2d165a85fdc775d1d4d43706c95381e0cf774
dang-nh <thanhnguyen200399@gmail.com>
2022-06-02 13:28:32 +0700

    Update run Script

---
+++ run/PPMatching/VAIPE-PP_02/FinalScrip/PPMatching-Graph-Mobilenetv3-NonPretrain-SBertMulti.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 python3 -u train.py --run-name="VAIPE-02-Graph-Resnet18-NonPretrain-SBertMulti" --image-model-name="mobilenet_v3_small" --image-embedding=1000 --image-trainable=True --image-pretrained=False --matching-criterion="ContrastiveLoss" --text-model-name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2" --text-embedding=384 --train-batch-size=4 --val-batch-size=1 --data-folder="/mnt/disk1/vaipe-thanhnt/EMED-Prescription-and-Pill-matching/data/VAIPE-PP_02/"
+ 
---
+++ run/PPMatching/VAIPE-PP_02/FinalScrip/PPMatching-Graph-Mobilenetv3-Pretrain-SBertMulti.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=1 python3 -u train.py --run-name="VAIPE-02-Graph-Resnet18-Pretrain-SBertMulti" --image-model-name="mobilenet_v3_small" --image-embedding=1000 --image-trainable=True --image-pretrained=True --matching-criterion="ContrastiveLoss" --text-model-name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2" --text-embedding=384 --train-batch-size=4 --val-batch-size=1 --data-folder="/mnt/disk1/vaipe-thanhnt/EMED-Prescription-and-Pill-matching/data/VAIPE-PP_02/"
+ 
